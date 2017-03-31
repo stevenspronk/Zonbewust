44805adb4666380d0c8e6cc779e41c6bb99f5155
Fab1Man <fabiancamps@gmail.com>
2017-03-31 17:12:48 +0000

    Initiele basis voor Inlogscherm, toevoegen scherm. Geen functionaliteit

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,9 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/m/Button",
+	"sap/m/Dialog",
+	"sap/ui/model/resource/ResourceModel"
+], function(Controller, MessageToast, Button, Dialog, ResourceModel) {
 	"use strict";
 
 	return Controller.extend("Zonbewust.controller.Login", {
 
+		onInit: function() {
+			//Resource model opbouwen en vastleggen in de SAP.UI Core, model i18n.
+			var resourceModel = new ResourceModel({
+				bundleName: "Zonbewust.controller.i18n"
+			});
+			sap.ui.getCore().setModel(resourceModel, "i18n");
+		},
+
+		onAddAccount: function() {
+			//Resource bundle ophalen
+			var resourceBundle = sap.ui.getCore().getModel("i18n").getResourceBundle();
+
+			//Account toevoegen Dialog popup
+			this._oDialog = null;
+
+			if (!this._oDialog) {
+				this._oDialog = sap.ui.xmlfragment("Zonbewust.view.AddAccount", this);
+
+			}
+			this.getView().addDependent(this._oDialog);
+			this._oDialog.open();
+		}
 	});
 });
