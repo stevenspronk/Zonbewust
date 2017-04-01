335b93c98b377aed28c2eef3ca09b519327bdb9c
stevenspronk <stevenspronk@gmail.com>
2017-04-01 10:15:41 +0000

    push

--- webapp/controller/AddAccount.controller.js
+++ webapp/controller/AddAccount.controller.js
@@ -6,38 +6,46 @@ sap.ui.define([
 	return Controller.extend("Zonbewust.controller.AddAccount", {
 
 		onInit: function() {
-		
+
+			var data = {
+				"name": null,
+				"age": null,
+				"panda_gender": null,
+				"panda_name": null,
+				"bamboo": null,
+				"panda_color": "green"
+			};
+
+			var oModel = new sap.ui.model.json.JSONModel(data);
+			this.getView().setModel(oModel);
 		},
 
-		onAfterRendering: function() {
-			// var data = {
-			// 	"name": "null",
-			// 	"age": null,
-			// 	"panda_gender": null,
-			// 	"panda_name": null,
-			// 	"bamboo": null
-			// };
-			var oUsers = this.getView().getModel("oUsers");
-			oUsers.refresh(true, true);
-				
-			var oContext = oUsers.createEntry("/Z_User");
-
-			this.getView().byId("formChangeAccount").setBindingContext(oContext, "oUsers");
-		},
-		
 		onSave: function(oEvent) {
+			var self = this;
 			var oUsers = this.getView().getModel("oUsers");
+			var oModel = this.getView().getModel();
+
+			oUsers.createEntry("/Z_User", {
+				properties: oModel.getData()
+			});
 
 			if (oUsers.hasPendingChanges()) {
 				oUsers.submitChanges({
 					success: function(oData) {
 						sap.m.MessageToast.show("Succesfully saved");
+						var oRouter = sap.ui.core.UIComponent.getRouterFor(self);
+						oRouter.navTo("login");
 					},
 					error: function(oError) {
 						sap.m.MessageToast.show(oError.message);
+						var oRouter = sap.ui.core.UIComponent.getRouterFor(self);
+						oRouter.navTo("login");
 					}
 				});
+			} else {
+				sap.m.MessageToast.show("No changes");
 			}
+
 		}
 
 	});
