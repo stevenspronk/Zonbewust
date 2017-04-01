5502ff5833012d0abf5289fd5c22e04328a98024
Joury Jonkergouw <joury@zoit.nl>
2017-04-01 10:16:57 +0000

    Update

--- webapp/controller/Leaderboard.controller.js
+++ webapp/controller/Leaderboard.controller.js
@@ -8,6 +8,23 @@ sap.ui.define([
 			var oModel = this.getView().getModel("oUsers");
 			this.getView().setModel(oModel);
 			oModel.updateBindings();
+		},
+		
+		onBack  : function() {
+				//Start Event dat leidt naar het GameScreen
+			//Dit kan via de AddAccount Dialog of door direct een van de bestaande accounts te kiezen
+			if (this._oDialog) {
+				this.onCloseAccount();	
+			}
+			
+			//Event voor navigatie-routing naar Gamescreen
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("login");
+		},
+		
+		imageFormatter : function(value) {
+    		var imageSrc = "images/faces/1_" + value + ".svg";
+    		return imageSrc;
 		}
 	});
 
